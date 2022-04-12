74684200d68cc309418a56053559565471c3cafd
Redoan <nazmulhaqueredoan@gmail.com>
2022-04-12 22:36:42 +0600

    styling message_client

--- message_client/src/main/java/com/example/message_client/HelloApplication.java
+++ message_client/src/main/java/com/example/message_client/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.message_client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 478, 400);
-        stage.setTitle("Hello!");
+        Image icon = new Image("https://cdn-icons-png.flaticon.com/512/1041/1041916.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Client");
         stage.setScene(scene);
         stage.show();
     }
